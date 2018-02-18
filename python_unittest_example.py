33709cbcbfca3a15009b78fcf7af2fb284758bf2
hallelujar <yazhou@seas.upenn.edu>
2018-02-18 08:24:07 -0500

    add unittest example

---
+++ python_unittest_example.py
@@ -0,0 +1,56 @@
+import unittest
+
+### Class to be tested
+class Dict(dict):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
+### Test code
+class TestDict(unittest.TestCase):
+    def test_init(self):
+        d = Dict(a=1, b = 'test')
+        self.assertEqual(d.a, 1)
+        self.assertEqual(d.b, 'test')
+        self.assertTrue(isinstance(d,dict))
+
+    def test_key(self):
+        d = Dict()
+        d['key'] = 'value'
+        self.assertEqual(d.key, 'value')
+
+    def test_attr(self):
+        d = Dict()
+        d.key = 'value'
+        self.assertTrue('key' in d)
+        self.assertEqual(d['key'], 'value')
+
+    def test_keyerror(self):
+        d = Dict()
+        with self.assertRaises(KeyError):
+            value = d['empty']
+
+    def test_attrerror(self):
+        d = Dict()
+        with self.assertRaises(AttributeError):
+            value = d.empty
+
+    def setUp(self):
+        print('setUp...')
+
+    def tearDown(self):
+        print('tearDown...')
+
+
+### Run the test
+if __name__ == '__main__':
+    unittest.main()
